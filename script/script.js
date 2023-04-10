ec395c3d3ab700ca84263b92ce9140e6da25484f
Shirin <shirin.mohajer@gmail.com>
2023-04-10 17:06:08 -0400

    update

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 "use strict";
 
 //You are going to need your own spesific API key - follow the instruction in the README.md file
-const parksUrl =
-  "https://developer.nps.gov/api/v1/parks?stateCode=MD&api_key=Z0JYxq0RVQGYlSzaKoAJxcg07PXUPPDTVAmGbhGa";
+//const parksUrl =
+// "https://developer.nps.gov/api/v1/parks?stateCode=MD&api_key=Z0JYxq0RVQGYlSzaKoAJxcg07PXUPPDTVAmGbhGa";
 
+const parksUrl = "https://developer.nps.gov/api/v1/parks";
+const api_key = "Z0JYxq0RVQGYlSzaKoAJxcg07PXUPPDTVAmGbhGa";
+const stateCode = " ";
 const updateUISuccess = function (data) {
   const parseData = JSON.parse(data);
   console.log(parseData);
@@ -22,13 +25,20 @@ const responseMethod = function (httpRequest) {
     }
   }
 };
+
 const createRequest = function (url) {
   const httpRequest = new XMLHttpRequest(url);
-
   httpRequest.addEventListener("readystatechange", (url) => responseMethod(httpRequest));
   httpRequest.open("Get", url);
   httpRequest.send();
   console.log(httpRequest);
 };
 
-createRequest(parksUrl);
+const checkCompletion = function () {
+  console.log("Checking if state has value before sending the request.");
+  if (stateCode !== "") {
+    console.log("This shouldn't run");
+    const requestUrl = createRequest(`${parksUrl}?stateCode=${stateCode}&api_key=${api_key}`);
+  }
+};
+checkCompletion();
