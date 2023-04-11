655884d7d41769a909b51b2944b9eb6e8ddc9fb7
Shirin <shirin.mohajer@gmail.com>
2023-04-10 22:26:53 -0400

    project draft

--- script/script.js
+++ script/script.js
@@ -3,13 +3,22 @@
 //You are going to need your own spesific API key - follow the instruction in the README.md file
 //const parksUrl =
 // "https://developer.nps.gov/api/v1/parks?stateCode=MD&api_key=Z0JYxq0RVQGYlSzaKoAJxcg07PXUPPDTVAmGbhGa";
-
+//https://www.nps.gov/subjects/developer/guides.htm#CP_JUMP_5617008
 const parksUrl = "https://developer.nps.gov/api/v1/parks";
 const api_key = "Z0JYxq0RVQGYlSzaKoAJxcg07PXUPPDTVAmGbhGa";
-const stateCode = " ";
+const stateCode = "MD";
+const into = document.querySelector(".container");
+
 const updateUISuccess = function (data) {
   const parseData = JSON.parse(data);
-  console.log(parseData);
+  console.log("Josn format", parseData);
+
+  let container = document.createElement("div");
+  let pElem = document.createElement("p");
+  pElem.textContent = JSON.stringify(parseData);
+  container.appendChild(pElem);
+  into.replaceChild(container, document.querySelector(".container div"));
+  console.log(container);
 };
 const updateUIError = function (error) {
   console.log(error);
